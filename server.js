a0b54ba25dc621a0d426290052c04acaa9c2752c
yaegashi0314 <a250893h@reitaku.jp>
2026-07-10 09:54:27 +0900

    fix express static

--- server.js
+++ server.js
@@ -47,3 +47,11 @@ app.post("/api/chat", async (req, res) => {
 app.listen(3000, () => {
   console.log("http://localhost:3000");
 });
+const express = require("express");
+const app = express();
+
+app.use(express.static("."));
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
